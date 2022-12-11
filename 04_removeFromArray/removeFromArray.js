659be15ef94e8175f4dccde75d2ec14b0e0e87ec
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2022-12-11 18:16:34 +0600

    Completed problem 01 to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...num) {
+    let removeItems = [...num];
+    return arr.filter(item => !removeItems.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3], 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
