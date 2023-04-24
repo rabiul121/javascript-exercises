3c27d2489bcba655a6a542d0313669074001cfe0
Md. Rabiul Islam <91551048+rabiul121@users.noreply.github.com>
2023-04-24 22:36:04 +0600

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const serial = parseInt(num);
+    if (serial < 0) {
+        return "OOPS";
+    }
+    else if (serial <= 1) {
+        return serial;
+    } else {
+        return fibonacci(serial - 1) + fibonacci(serial - 2);
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
